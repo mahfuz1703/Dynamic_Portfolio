6e2be0de60a3fb86fa62cd1fd64d62c55573aad7
Mahfuz Mia <sayhello.mahfuz@gmail.com>
2024-06-18 23:31:56 +0600

    deploy

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
-
-# Create and activate a virtual environment
-python3.9 -m venv venv
-source venv/bin/activate
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Run other build steps
-# ...
+ echo "BUILD START"
+ python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py collectstatic --noinput --clear
+ echo "BUILD END"
