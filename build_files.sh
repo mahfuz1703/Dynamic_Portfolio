f656071543f5ad169a76d90c2e7fe6ac68e4ceec
Mahfuz Mia <sayhello.mahfuz@gmail.com>
2024-06-18 23:09:41 +0600

    deploy

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
+# Create and activate a virtual environment
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
-pthon3.9 manage.py collectstatic
+
+# Run other build steps
+# ...
